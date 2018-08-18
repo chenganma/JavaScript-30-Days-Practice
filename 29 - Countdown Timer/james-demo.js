89af286a02e37a8389a99efa43b7e85b6dde7d6f
Cheng-An Ma <s84190kimo@yahoo.com.tw>
2018-08-18 14:10:55 -0500

    final

--- 29 - Countdown Timer/james-demo.js
+++ 29 - Countdown Timer/james-demo.js
@@ -1,18 +1,75 @@
+let countdown;
+
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
+
 function timer(seconds) {
+	//clear any exsiting times
+	clearInterval(countdown);
+
+
 	const now = Date.now();
 	const then = now + seconds * 1000;
-	console.log({now, then});
+	//console.log({now, then});
 
-	coountdown = setInterval(()=>{
+	displayTimeLeft(seconds);
+
+	displayEndTime(then);
+
+	countdown = setInterval(()=>{
 
 		const secondsLeft = Math.round((then - Date.now()) / 1000);
 		//check if we need to stop it
 		if(secondsLeft < 0){
-			clearInterval(coountdown);
+			clearInterval(countdown);
 			return;
 		}
-		console.log(secondsLeft);
+		//console.log(secondsLeft);
+
+		displayTimeLeft(secondsLeft);
 
 
 	}, 1000);
 }
+
+function displayTimeLeft(seconds){
+
+	const minutes = Math.floor(seconds / 60);
+	const remainSeconds = seconds % 60;
+
+	const display = `${minutes}:${remainSeconds < 10 ? '0' : ''}${remainSeconds}`;
+
+	timerDisplay.textContent = display;
+
+	//console.log({minutes, remainSeconds});
+}
+
+function displayEndTime(timestamp){
+
+	const end = new Date(timestamp);
+	const hour = end.getHours();
+	const minutes = end.getMinutes();
+	//const seconds = end.getSeconds();
+
+	endTime.textContent = `Be Back At ${hour > 12 ? (hour - 12) : hour}:${minutes < 10 ? '0' : ''}${minutes}`;
+
+}
+
+function startTimer(){
+	const seconds = parseInt(this.dataset.time);
+	timer(seconds);
+	//console.log(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click',startTimer));
+
+document.customForm.addEventListener('submit', function(e){
+
+	e.preventDefault();
+	const mins = this.minutes.value;
+	console.log(mins);
+	timer(mins*60);
+	this.reset();
+});
+
